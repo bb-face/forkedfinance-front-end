429a0d9ceeab3e1a56a219478592276be593de0f
stefano <stefano1990@gmail.com>
2024-05-06 10:04:46 +0200

    Fixed dashboard scrollabar problem

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,16 @@ import useNetworkChange from "./customHooks/useNetworkChange";
 function App() {
   useNetworkChange();
 
+  // TODO: it wouldn't take the height from the config, check:
+  //<main className="flex-grow pt-[header-height] pb-[footer-height] flex items-center justify-center overflow-auto"></main>
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow pt-[header-height] pb-[footer-height] flex items-center justify-center">
+      <Navbar className="fixed top-0 left-0 w-full z-10" />
+      <main className="flex-grow pt-32 pb-14 flex items-center justify-center overflow-auto">
         <Outlet />
       </main>
-      <Footer />
+      <Footer className="fixed bottom-0 left-0 w-full z-10" />
     </div>
   );
 }
