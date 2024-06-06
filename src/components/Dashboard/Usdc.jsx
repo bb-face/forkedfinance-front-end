fbf85b9b3ea7647385137b7ddc94620249a65789
Yury Rodionov <yuryrodionov@Yurys-MacBook-Pro.local>
2024-06-06 19:20:07 +0100

    test permit

--- src/components/Dashboard/Usdc.jsx
+++ src/components/Dashboard/Usdc.jsx
@@ -28,60 +28,59 @@ function Usdc({
   const [amount, setAmount] = useState(0);
   const currentAddress = useRecoilValue(walletAddressAtom);
 
-  // const value = ethers.constants.MaxUint256;
 
-  // async function permitSigned(signer, token, spender, timestamp) {
-  //   const deadline = timestamp + 86400;
-  //   const [nonce, name, version, chainId, ownerAddress] = await Promise.all([
-  //     token.nonces(signer.getAddress()),
-  //     token.name(),
-  //     "1",
-  //     signer.getChainId(),
-  //     signer.getAddress(),
-  //   ]);
+  async function permitSigned(signer, token, spender, timestamp) {
+    const deadline = timestamp + 86400;
+    const [nonce, name, version, chainId, ownerAddress] = await Promise.all([
+      token.nonces(signer.getAddress()),
+      token.name(),
+      "1",
+      signer.getChainId(),
+      signer.getAddress(),
+    ]);
   
-  //   const typedData = [
-  //     {
-  //       name,
-  //       version,
-  //       chainId,
-  //       verifyingContract: token.address,
-  //     },
-  //     {
-  //       Permit: [
-  //         {
-  //           name: "owner",
-  //           type: "address",
-  //         },
-  //         {
-  //           name: "spender",
-  //           type: "address",
-  //         },
-  //         {
-  //           name: "value",
-  //           type: "uint256",
-  //         },
-  //         {
-  //           name: "nonce",
-  //           type: "uint256",
-  //         },
-  //         {
-  //           name: "deadline",
-  //           type: "uint256",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       owner: ownerAddress,
-  //       spender,
-  //       value,
-  //       nonce,
-  //       deadline,
-  //     },
-  //   ];
+    const typedData = [
+      {
+        name,
+        version,
+        chainId,
+        verifyingContract: token.address,
+      },
+      {
+        Permit: [
+          {
+            name: "owner",
+            type: "address",
+          },
+          {
+            name: "spender",
+            type: "address",
+          },
+          {
+            name: "value",
+            type: "uint256",
+          },
+          {
+            name: "nonce",
+            type: "uint256",
+          },
+          {
+            name: "deadline",
+            type: "uint256",
+          },
+        ],
+      },
+      {
+        owner: ownerAddress,
+        spender,
+        maxUint,
+        nonce,
+        deadline,
+      },
+    ];
   
-  //   return typedData;
-  // }
+    return typedData;
+  }
 
   const depositUSDC = async () => {
     if (window.ethereum?.isMetaMask) {
