f1618b874a9f819cf7b3f1e22f850bf44f89e80f
Yury Rodionov <yuryrodionov@Yurys-MacBook-Pro.local>
2024-06-08 22:23:02 +0100

    home page pic, change of desriptions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,38 @@ import { AiOutlineRise } from "react-icons/ai";
 import { IoSend } from "react-icons/io5";
 import { RiTakeawayFill } from "react-icons/ri";
 
+import tutoBear from "../assets/TutoBear.png";
 
 function Home() {
 	return (
 		<div className="flex flex-col h-screen justify-center items-center  text-center px-4">
-			<h1 className="text-5xl font-bold text-primary leading-tight mb-4">
-				The first web3 Meme Banking Protocol
+			<h1 className="text-white text-4xl font-bold text-primary leading-tight mb-4">
+				The First and Only Protocol Memecoin
 			</h1>
-			<div className="text-base text-white max-w-md mb-8">
+
+			<div  className="flex flex-row w-screen justify-center items-center">
+          		<img src={tutoBear} alt="Tuto Bear" className="tutoBear" />
+
+				<div className="flex text-base text-white items-center  max-w-md mb-8">
 				<ul className="list-none space-y-2">
 					<li className="text-white text-lg flex items-center">
-						<CiWallet className="mr-2" /> Connect Wallet
+						<CiWallet className="mr-2" /> Fair Launch
+
 					</li>
 					<li className="text-white text-lg flex items-center">
-						<AiOutlineRise className="mr-2" /> Deposit USDC to Earn APR
+						<RiTakeawayFill className="mr-2" /> Token Utility
 					</li>
 					<li className="text-white text-lg flex items-center">
-						<IoSend className="mr-2" /> Send USDC without Gas or Fees
+						<AiOutlineRise className="mr-2" /> LP Burnt 
 					</li>
 					<li className="text-white text-lg flex items-center">
-						<RiTakeawayFill className="mr-2" /> Withdraw USDC
+						<IoSend className="mr-2" /> Ownership Renounced
 					</li>
 				</ul>
+				
+				</div>
 			</div>
+			
 			
 		</div>
 		
