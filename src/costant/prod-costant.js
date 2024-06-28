db7fbd1a669ca4a7eb364adbb03ad1c7ecbb036d
Yury Rodionov <yuryrodionov@Yurys-MacBook-Pro.local>
2024-06-28 16:04:42 +0100

    new addresses and abis

--- src/costant/prod-costant.js
+++ src/costant/prod-costant.js
@@ -4,12 +4,12 @@ export const USDCAddress_back = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const bankAddress = "0x3149496ED8C90FC2418b3dD389ca606b87d23D45";
 export const USDCApprovaAmount =
   "115792089237316195423570985008687907853269984665640564039457584007913129639935";
-export const tutoAddr = "0x79F34b800A0B41608e322d57f7887971cFDE35C5";
+export const tutoAddr = "0x5913fC4A8caA54d9bCDE06076A49462b864Ac97C";
 export const usdcAddr ="0x8c7A265C1C40F65A6F924207fa859da29b581c2B";
 export const UsdcAddr ="0x8c7A265C1C40F65A6F924207fa859da29b581c2B";
-export const rewardRouterAddr ="0x9573b1E2a552F3fc057654718FB99483F1B20260";
-export const feeUsdcAddr = "0xd45c2096ea9811daa3bd6a00c63Cf96A8e715D9B";
-export const feeTutoAddr = "0x43B7b356a1bEadBd320cdCD8846b8d42099660fF";
+export const rewardRouterAddr ="0x3b78D52d9d45096aB698f8b01e0a8954EcDa6512";
+export const feeUsdcAddr = "0x4cdfE10a7B1635710C7DF4c27B5b17129B03fe5E";
+export const feeTutoAddr = "0x58650AE227d16Ee145314876610e94f442278CAE";
 
 export const usdcDecimals = 10 ** 6;
 export const decimals = 10 ** 18;
