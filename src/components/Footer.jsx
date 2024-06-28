9db964453602180c5cab0710cff8e435e6c3d855
stefano <stefano1990@gmail.com>
2024-06-28 13:36:40 +0200

    Responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,55 +5,63 @@ import uniswapLogo from "../assets/uniswapLogo.png";
 import dexScreenerLogo from "../assets/dexScreenerLogo.png";
 
 function Footer() {
-	return (
-		<footer className="bg-primary w-full py-4 px-8 fixed bottom-0 z-50 flex items-center justify-center">
-			<div className="flex items-center space-x-8">
-				<a
-					href="https://twitter.com/Tuto_Protocol"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaTwitter size={24} />
-				</a>
-				<a
-					href="https://t.me/+eJFVxOHHzuY1YmI8"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaTelegram size={24} />
-				</a>
-				<a
-					href="https://github.com/yury-ff"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaGithub size={24} />
-				</a>
-				<a
-					href="https://etherscan.io/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={etherscanLogo} alt="etherscan" className="w-6 " />
-				</a>
-				<a
-					href="https://app.uniswap.org/swap?chain=sepolia"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-		  			<img src={uniswapLogo} alt="uniswap" className="w-6 bg-white border rounded-full" />
-				</a>
-				<a
-					href="https://dexscreener.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={dexScreenerLogo} alt="dexscreener" className="w-6 "/>
-				</a>
-				<p className="text-sm">© 2024 Tuto by ForkedFinance</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="bg-primary py-4 w-full fixed bottom-0 z-50 flex items-center justify-evenly sm:justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="flex items-center space-x-8 justify-center">
+          <a
+            href="https://twitter.com/Tuto_Protocol"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter size={24} />
+          </a>
+          <a
+            href="https://t.me/+eJFVxOHHzuY1YmI8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegram size={24} />
+          </a>
+          <a
+            href="https://github.com/yury-ff"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={24} />
+          </a>
+        </div>
+        <div className="flex items-center space-x-8 justify-center md:ml-3">
+          <a
+            href="https://etherscan.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={etherscanLogo} alt="etherscan" className="w-6 " />
+          </a>
+          <a
+            href="https://app.uniswap.org/swap?chain=sepolia"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={uniswapLogo}
+              alt="uniswap"
+              className="w-6 bg-white border rounded-full"
+            />
+          </a>
+          <a
+            href="https://dexscreener.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={dexScreenerLogo} alt="dexscreener" className="w-6 " />
+          </a>
+        </div>
+      </div>
+      <p className="text-sm md:ml-6">© 2024 Tuto by ForkedFinance</p>
+    </footer>
+  );
 }
 
 export default Footer;
